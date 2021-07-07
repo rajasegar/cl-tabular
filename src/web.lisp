253e5826d444fb5c9df4a881f0d255f29a0c1c81
Rajasegar Chandran <rajasegar.c@gmail.com>
2021-07-07 15:58:41 +0530

    chore: add search route

--- src/web.lisp
+++ src/web.lisp
@@ -32,6 +32,9 @@
 (defun random-elt (mylist)
   (elt mylist (random (length mylist))))
 
+;; Clear the list
+(setf *foods* nil)
+
 (dotimes (i 100)
   (push (list :id (+ 1 i) :name (random-elt *dishes*) :cuisine (random-elt *cuisines*) :rating (+ 1 (random 5)) :price (+ 1 (random 100))) *foods*))
 
@@ -47,9 +50,28 @@
 (defroute "/" (&key _parsed)
   (format t "_parsed = ~a~%" _parsed)
   (let ((start (parse-integer (or (query-param "start" _parsed) "0")))
-	(direction (query-param "direction" _parsed))
-	(sort-by (query-param "sort-by" _parsed)))
-  (render #P"index.html" (list :foods (slice-list start)))))
+        (direction (query-param "direction" _parsed))
+        (sort-by (query-param "sort-by" _parsed)))
+    (render #P"index.html"
+            (list
+             :foods (slice-list start)
+             :total (length *foods*)))))
+
+(defun filter-foods (query)
+  (remove-if #'(lambda (food)
+                 (let ((name (getf food :name)))
+                   (if (search query name :test #'char-equal)
+                       nil
+                       t))) *foods*))
+
+(defroute ("/search" :method :POST) (&key _parsed)
+  (format t "_parsed = ~a~%" _parsed)
+  (let* ((query (cdr (assoc "query" _parsed :test #'string=)))
+        (filtered-foods (filter-foods query)))
+    (render #P"_search.html"
+            (list
+             :foods filtered-foods
+             :total (length filtered-foods)))))
 
 ;;
 ;; Error pages
